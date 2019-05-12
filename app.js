374102ba82fcd4b7c860ff06e6aaea6a8f9b6339
ruisoho <49654391+ruisoho@users.noreply.github.com>
2019-05-12 22:23:18 +0200

    debug

--- app.js
+++ app.js
@@ -98,11 +98,11 @@ document.querySelector('.btn-new').addEventListener("click", int)
             document.getElementById("current-1").textContent = "0";
             document.getElementById("name-0").textContent = "Player 1";
             document.getElementById("name-1").textContent = "Player 2";
-            document.querySelector(".player-0-panel-").classList.remove("winner");
-            document.querySelector(".player-1-panel-").classList.remove("winner");
-            document.querySelector(".player-0-panel-").classList.remove("active");
-            document.querySelector(".player-1-panel-").classList.remove("active");
-            document.querySelector(".player-0-panel-").classList.add("active");
+            document.querySelector(".player-0-panel").classList.remove("winner");
+            document.querySelector(".player-1-panel").classList.remove("winner");
+            document.querySelector(".player-0-panel").classList.remove("active");
+            document.querySelector(".player-1-panel").classList.remove("active");
+            document.querySelector(".player-0-panel").classList.add("active");
     
     };
 
